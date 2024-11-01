c49bcbcb6e3c644df12be4c14d83a4ca28379d6e
AdityaPoonia <145849577+AdityaPoonia890@users.noreply.github.com>
2024-11-02 03:23:52 +0530

    first commit

---
+++ src/main/java/com/aditya/ecom_project/model/Product.java
@@ -0,0 +1,45 @@
+package com.aditya.ecom_project.model;
+
+import java.sql.Date;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Component
+public class Product {
+
+ @Id  
+ @GeneratedValue(strategy = GenerationType.IDENTITY) 
+ private int id;
+ private String name;
+ private String description;
+ private String brand;
+ private int price;
+ private String category;
+ 
+ private Date releaseDate;
+ private boolean productAvailable;
+ private int stockQuantity;
+
+ @Lob
+ @Column(name="image_date", nullable=false, columnDefinition="blob")
+ private byte[] imageDate;
+
+ private String imageName;
+ private String imageType;
+
+}
